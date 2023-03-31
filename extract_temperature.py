cddc8bc13660b52330bab0f9ae9a4100f1164d17
anjonas <anjonas@student.ethz.ch>
2023-03-31 18:09:40 +0200

    switched to using the new generated plot shapes

--- extract_temperature.py
+++ extract_temperature.py
@@ -2,11 +2,12 @@
 from TemperatureExtractor import TemperatureExtractor
 
 workdir = 'Z:/Public/Jonas/007_Thermo'
+workdir = '/home/anjonas/public/Public/Jonas/007_Thermo'
 
 
 def run():
     dirs_to_process = f'{workdir}/TestImages'
-    path_geojson = f'{workdir}/ESWW006_plot_shapes_w_id.geojson'
+    path_geojson = f'{workdir}/ESWW006_plot_shapes_generated.geojson'
     dir_output = f'{workdir}/Output'
     temperature_extractor = TemperatureExtractor(
         dirs_to_process=dirs_to_process,
